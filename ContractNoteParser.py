7655cf372ddab02e1e5de6220379f621e56b2ec8
Vikram Kumar <vikram17june@gmail.com>
2022-03-01 01:48:58 +0530

    MVP which consolidates all the holding in a CSV format

--- ContractNoteParser.py
+++ ContractNoteParser.py
@@ -4,13 +4,12 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 import tabula
 import csv
 
-
-def parse_file(fileName):
-    print(f'Hi, The file name is  {fileName}')
-    pdf_file = open(fileName,'rb')
+def parse_file(fileName,folderName):
+    print(f'Hi, The file name is  {folderName+fileName}')
+    pdf_file = open(folderName+fileName,'rb')
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
 
-    password ="VVVVVVV"
+    password ="VVVV"
     # Check if the opened file is actually Encrypted
     if read_pdf.isEncrypted:
         print ("File is password protected")
@@ -31,12 +30,26 @@ def parse_file(fileName):
 
     input = open("samplefiles/output.csv", 'rt')
     output = open("samplefiles/output_updated.csv", 'wt')
+    second_output = open("consolidated_holdings/consolidated_holdings.csv", 'a')
+
     writer = csv.writer(output)
+    second_writer = csv.writer(second_output)
     toggle=False;
+    headerIdentified=False;
+
+
     for row in csv.reader(input):
-        print (row[0]);
+        if (row[0]=="Trade Date"):
+            print (row[1]);
+            tradeDate=row[1]
         if (row[0]=="Segment"):
             toggle=True;
+
+        # Remove case where the summary table is multipage
+        if (row[0]=="BSE-Cash"):
+            toggle=False;
+        if (row[0]=="NSE-Cash"):
+            toggle=False;
 
         if toggle:
             if (row[0] == "Sub Total" or row[0] == "Total"):
@@ -44,7 +57,28 @@ def parse_file(fileName):
                 #writer.writerow(','.join(row))
                 writer.writerow([' ']+row)
             else:
-                writer.writerow(row)
+                # check if header has alredy been written then do not write it again
+                if (row[0] == "Segment"):
+                    if (headerIdentified==False):
+                        headerIdentified=True;
+                        writer.writerow(row)
+                else:
+                    writer.writerow(row)
+                    # Need to extract the ISIN from the below value
+                    security_description = row[1]
+                    isin=row[1].split("Cash-", 1)[1].strip()
+                    bought = row[2]
+                    sold = row[3]
+                    total_tx_cost=row[9]
+
+                    print("ISIN : "+isin)
+                    print("Security Description:"+security_description)
+                    print("Bought:" + bought)
+                    print("Sold:" + sold)
+                    print("Total Amount:" + total_tx_cost)
+
+                    second_writer.writerow([tradeDate,isin,security_description,bought,sold,total_tx_cost])
+
     input.close()
     output.close()
 
@@ -54,5 +88,5 @@ def parse_file(fileName):
     # Remove the full csv file
     os.remove('samplefiles/output.csv')
 
-
-
+    # Possibly remove the partial csv file as well
+    #os.remove('samplefiles/output_updated.csv')
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from ContractNoteParser import *
+from os import listdir
+from os.path import isfile, join
+from shutil import copyfile
+from os.path import exists
 
 
-def print_hi(name):
+
+def parse_files(folderName):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    parse_file(name);
+    print(f'Hi, {folderName}')  # Press ⌘F8 to toggle the breakpoint.
+    filesname = list_files(folderName);
+
+    for p in filesname:
+        print ("First File"+p)
+        # check if file has already been processed
+        if (exists('processed/' + p)):
+            print ("File Already Processed - Ignoring")
+        else:
+            parse_file(p,folderName);
+            # copy file to processed folder
+            copyfile(folderName + p, 'processed/' + p)
+
+
+def list_files(input_path):
+    onlyfiles = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+    print ("List of files identified are...")
+    for p in onlyfiles:
+        print (p)
+    return onlyfiles
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('samplefiles/1932126_1932126NCommonContractNote15160523632112012016_12012016.pdf')
+    parse_files('inputfiles/')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
